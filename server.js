4db7f03e1727df021d8bd0f089da65809ead43f6
shashank chourasia01 <shashankchourasia0829@gmail.com>
2026-02-27 21:30:49 +0530

    QFF

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:3000',
-  'https://food-ordering-frontend.vercel.app',  // Replace with your actual frontend URL
+  'https://food-ordering-frontend.vercel.app/',  
   'https://food-fox-backend.onrender.com'
 ];
 
