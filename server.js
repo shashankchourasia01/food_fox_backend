f2933e4c121f2fb26ccf816e5d279cbce21afe52
shashank chourasia01 <shashankchourasia0829@gmail.com>
2026-02-27 23:20:20 +0530

    add frontend url

--- server.js
+++ server.js
@@ -29,7 +29,8 @@ const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:3000',
-  'https://food-ordering-frontend.vercel.app/',  
+  'https://food-ordering-frontend.vercel.app/',
+  'https://food-fox-five.vercel.app',   
   'https://food-fox-backend.onrender.com'
 ];
 
